02676461f14ed41c1d24756061842bd431cb5111
esh8851 <esh8851@gmail.com>
2024-10-02 15:23:49 +0900

    interceptor

---
+++ daysix/src/main/java/com/seon/common/config/WebMvcConfigurerImpl.java
@@ -0,0 +1,29 @@
+package com.seon.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.seon.common.interceptor.CheckLoginSessionInterceptor;
+
+@Configuration
+public class WebMvcConfigurerImpl implements WebMvcConfigurer {
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new CheckLoginSessionInterceptor())
+//				.order(1)
+				.addPathPatterns("/*/*/*/*Xdm*")
+				.excludePathPatterns(
+//						"/resources/**",
+						"/adt/**",
+						"/xdm/**",
+						"/v1/infra/login/loginXdmSignIn",
+						"/v1/infra/member/signinXdmProc",
+						"/v1/infra/member/signoutXdmProc"
+//						"/v1/infra/member/signinXdmForm",
+//						"/v1/infra/member/signinXdmProc"
+		);
+	}
+
+}
--- daysix/src/main/java/com/seon/common/constans/Constants.java
+++ daysix/src/main/java/com/seon/common/constans/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
 	public static final int ROW_NUM_TO_SHOW = 5;
 	public static final int PAGE_NUM_TO_SHOW = 5;
 	
+	public static final String URL_LOGINFORM = "/v1/infra/login/loginXdmSignIn";
+	
 }
